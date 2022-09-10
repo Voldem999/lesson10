23db064388fc78aad21ce6e2222d2819526e62a7
Ilya201091 <ZiK201091@ya.ru>
2022-09-10 15:28:42 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        PersonService personService = new PersonService();
+        Person sarah = new Person("Sarah", 30);
+        if (personService.isAdult(sarah)) {
+            System.out.println("Иди в бар");
+        } else {
+            System.out.println("Иди в школу");
+        }
+        System.out.println(sarah);
+        sarah.increaseAge(30);
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean isAdult() {
+        return this.getAge() > 18;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void increaseAge(int increment) {
+        if (increment < 0) {
+            throw new IllegalArgumentException("Инкремент не может быть отрицательным");
+        }
+        this.age = age + increment;
+    }
+
+    public String toString() {
+        return "Имя " + this.name + " Возраст " + this.age;
+    }
+}
